734526ad08b977ed1da16fabf029eae35ec5dc68
mager19 <mager19@gmail.com>
2024-09-17 20:53:30 -0500

    hero added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require("tailwindcss/plugin");
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./**/*.{html,js}",
+    '!./node_modules/**/*.{html,js,php}',
+  ],
   theme: {
     extend: {
       colors: {
@@ -16,28 +19,28 @@ module.exports = {
         'redaccent': '#FF7262',
       },
       fontFamily: {
-        "space": ["Space Mono", "sans-serif"],
+        "space": ["Space Mono", "system-ui"],
         "workSans": ["Work Sans", "sans-serif"],
       },
       spacing: {
         '17': '4.5rem',
       },
       fontSize: {
-        'h1-space': ['4.1875rem', { fontFamily: 'space', lineHeight: '110%' }],
-        'h2-space': ['3.25rem', { fontFamily: 'space', lineHeight: '110%' }],
-        'h3-space': ['2.375rem', { fontFamily: 'space', lineHeight: '120%' }],
-        'h4-space': ['1.75rem', { fontFamily: 'space', lineHeight: '140%' }],
-        'h5-space': ['1.375rem', { fontFamily: 'space', lineHeight: '160%' }],
-        'body-workSans': ['1.375rem', { fontFamily: 'workSans', lineHeight: '160%' }],
-        'base-space': ['1rem', { fontFamily: 'space', lineHeight: '140%' }],
-        'base-workSans': ['1rem', { fontFamily: 'workSans', lineHeight: '140%' }],
-        'button': ['1rem', { fontFamily: 'workSans', lineHeight: '0', letterSpacing: '10%' }],
-        'caption': ['0.75rem', { fontFamily: 'workSans', lineHeight: '110%' }],
-        'h1-workSans': ['4.1875rem', { fontFamily: 'workSans', lineHeight: '110%' }],
-        'h2-workSans': ['3.25rem', { fontFamily: 'workSans', lineHeight: '110%' }],
-        'h3-workSans': ['2.375rem', { fontFamily: 'workSans', lineHeight: '120%' }],
-        'h4-workSans': ['1.75rem', { fontFamily: 'workSans', lineHeight: '140%' }],
-        'h5-workSans': ['1.375rem', { fontFamily: 'space', lineHeight: '160%' }],
+        'h1-space': ['4.1875rem', { lineHeight: '110%' }],
+        'h2-space': ['3.25rem', { lineHeight: '110%' }],
+        'h3-space': ['2.375rem', { lineHeight: '120%' }],
+        'h4-space': ['1.75rem', { lineHeight: '140%' }],
+        'h5-space': ['1.375rem', { lineHeight: '160%' }],
+        'body-workSans': ['1.375rem', { lineHeight: '160%' }],
+        'base-space': ['1rem', { lineHeight: '140%', fontWeight: '400' }],
+        'base-workSans': ['1rem', { lineHeight: '140%', fontWeight: '400' }],
+        'button': ['1rem', { lineHeight: '0', letterSpacing: '10%' }],
+        'caption': ['0.75rem', { lineHeight: '110%' }],
+        'h1-workSans': ['4.1875rem', { lineHeight: '110%' }],
+        'h2-workSans': ['3.25rem', { lineHeight: '110%' }],
+        'h3-workSans': ['2.375rem', { lineHeight: '120%' }],
+        'h4-workSans': ['1.75rem', { lineHeight: '140%' }],
+        'h5-workSans': ['1.375rem', { lineHeight: '160%' }],
       },
       container: {
         center: true,
